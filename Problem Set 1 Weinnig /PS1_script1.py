24e1ccfee6ba894247b2a6d1e4d80aa322cb3a5f
Alexis Weinnig <AlexisWeinnig@84-38-35-4e-fb-bc.wireless.temple.edu>
2017-05-04 10:50:21 -0400

    Add existing file

---
+++ Problem Set 1 Weinnig /PS1_script1.py
@@ -0,0 +1,11 @@
+#Question 1
+
+x=input ("Enter a number, please.")
+double =  2*x
+print "Double of your number is ", double
+half = x*0.5
+print "Half of your number is ", half
+plus = x+50
+print "Your number plus 50 is", plus
+minus = x-50
+print "Your number minus 50 is", minus
